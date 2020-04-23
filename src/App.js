d33296bd8aa93e3c88151f00de1dd4ef4e5fa714
joseph <josephvarghesep@gmail.com>
2020-04-23 23:54:23 +1000

    updated menu items to naviagate with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/shop/hats' component={HatsPage}></Route>
+      </Switch>
     </div>
   );
+}
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
 }
 
 export default App;
